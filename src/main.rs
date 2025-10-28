eb4f22ef08d1cf2715ee5de67fceb74633f9d601
PrinzessBard <egorholikov234@gmail.com>
2025-10-28 16:27:22 +0300

    ds - binary_search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+fn binary_search(arr: &[u32], item: u32) -> Option<u32> {
+    let mut low = 0;
+    let mut high = arr.len() - 1;
+
+    while low <= high {
+        let mid = (low + high) / 2;
+        let guess: u32 = arr[mid];
+
+        if guess == item {
+            return Some(mid as u32);
+        } else if guess > item {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    None
+}
+
 fn main() {
-    println!("Hello, world!");
+    let item: u32 = 12;
+    dbg!(binary_search(&[1, 3, 5, 7, 9], item));
 }
